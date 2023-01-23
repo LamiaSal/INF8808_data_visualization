7dabcb7f7434ac0d77b21c8a1b6e30b35b17a021
lamia <lamia.salhi@eurecom.fr>
2023-01-22 19:25:00 -0500

    suppressed TPs

--- TP2/modes.py
+++
@@ -1,8 +0,0 @@
-'''
-    This file contains some constants to help manage the app's two
-    display modes, Percent and Count.
-'''
-
-MODES = dict(count='Count', percent='Percent')
-# We've changed the 'percent' column names according to the preprocessing
-MODE_TO_COLUMN = {MODES['count']: 'LineCount', MODES['percent']: 'LinePercent'} 
